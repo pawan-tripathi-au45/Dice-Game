0c6e72ec0472a582deccf031d6fac1d22c171531
pawan-tripathi-au45 <pbt5518@gmail.com>
2023-09-18 08:04:18 +0530

    push

---
+++ src/components/RoleDice.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const RoleDice = ({ roleDice, currentDice }) => {
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={roleDice}>
+        <img src={`/images/dice/dice_${currentDice}.png`} alt="dice 1" />
+      </div>
+      <p>Click on dice to roll</p>
+    </DiceContainer>
+  );
+};
+
+export default RoleDice;
+
+const DiceContainer = styled.div`
+  margin-top: 48px;
+  display: flex;
+  flex-directions: column;
+  align-items: center;
+
+  .dice {
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 24px;
+  }
+`;
